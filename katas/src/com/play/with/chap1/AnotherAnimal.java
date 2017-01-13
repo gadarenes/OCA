e900f8efe116ddc9c150379bc7826743649aee81
gadarenes <gadarene@gmx.com>
2017-01-13 17:28:57 +0200

    First things first: attribute, methods.

---
+++ katas/src/com/play/with/chap1/AnotherAnimal.java
@@ -0,0 +1,27 @@
+package com.play.with.chap1;
+
+public class AnotherAnimal {
+    
+	//leave it with the default access for now
+	String name; // this will be null 
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+    //full declaration of the method is called the method signature
+	// setName is not returning anything and it has one parameter 
+}
+
+
+
+
